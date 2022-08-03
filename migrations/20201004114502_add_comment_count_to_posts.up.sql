cd7f64d82d50e6d875185e3b964757ff8e49779a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 16:09:53 +0800

    Delete migrations directory

--- migrations/20201004114502_add_comment_count_to_posts.up.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE posts ADD COLUMN comment_count INTEGER NOT NULL DEFAULT 0;
-
-UPDATE posts AS p
-SET comment_count = (
-    SELECT count(*)
-    FROM comments AS c
-    WHERE c.post_id = p.id
-);
